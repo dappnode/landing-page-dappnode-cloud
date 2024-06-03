01d2450f0cb3d911d586459535144594122a3059
Mateumiralles <mateumiralles714@gmail.com>
2024-06-03 19:34:00 +0200

    carousel responsive

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,7 +6,7 @@ interface Slide {
   imgSrc: string;
 }
 
-const Slides: Slide[] = [
+const slides: Slide[] = [
   {
     title: "Solo Staking",
     text: "In case getting a hardware isn't an option for you - you can use Dappnode Cloud to host an Ethereum or Gnosis validator, thus participating in the network and earning rewards.",
@@ -34,12 +34,12 @@ export default function Carousel() {
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? Slides.length - 1 : currentIndex - 1;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === Slides.length - 1;
+    const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
@@ -51,14 +51,16 @@ export default function Carousel() {
   }, [currentIndex]);
 
   return (
-    <div className="mx-[10%] flex flex-row gap-52">
-      <div className="relative min-h-max w-2/5">
-        <div className=" flex h-full flex-col items-center justify-center gap-10 text-center">
-          <h2>{Slides[currentIndex].title}</h2>
-          <p>{Slides[currentIndex].text}</p>
+    <div className="mx-5 flex flex-col gap-10 lg:mx-[5%] lg:flex-row lg:gap-20 xl:mx-[10%] xl:gap-52">
+      <div className="relative flex min-h-max w-full flex-col  lg:w-2/5">
+        <div className="flex h-3/4 flex-col items-center justify-center gap-10 text-center">
+          <h2 className="lg:text-3xl xl:text-5xl">
+            {slides[currentIndex].title}
+          </h2>
+          <p className="lg:text-xl xl:text-2xl">{slides[currentIndex].text}</p>
         </div>
-        <div className="absolute bottom-32 left-[50%] mx-auto flex -translate-x-[50%] flex-row gap-5">
-          {Slides.map((slides, index) => (
+        <div className="mt-10 flex h-1/4 flex-row items-center justify-center gap-5">
+          {slides.map((slides, index) => (
             <button
               className={`h-3 w-3 rounded-full  ${index === currentIndex ? "bg-dappnodePurple" : "bg-gray-400"}`}
               onClick={() => setCurrentIndex(index)}
@@ -66,12 +68,12 @@ export default function Carousel() {
           ))}
         </div>
       </div>
-      <div className="relative w-3/5 overflow-hidden">
+      <div className="relative w-full overflow-hidden lg:w-3/5">
         <div
           className="flex transition-transform duration-300 ease-in-out"
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
-          {Slides.map((slide, index) => (
+          {slides.map((slide, index) => (
             <div key={index} className="w-full flex-shrink-0">
               <img
                 src={slide.imgSrc}
@@ -84,13 +86,13 @@ export default function Carousel() {
 
         <button
           onClick={prevSlide}
-          className="absolute bottom-0 right-14 transform  p-2 text-2xl text-black transition-transform duration-300 ease-in-out hover:-translate-x-3 "
+          className="absolute bottom-0 right-12 transform  p-2 text-2xl text-black transition-transform duration-300 ease-in-out hover:scale-125"
         >
           {"<"}
         </button>
         <button
           onClick={nextSlide}
-          className="absolute bottom-0 right-5  transform  p-2 text-2xl text-black transition-transform duration-300 ease-in-out hover:translate-x-3 "
+          className="absolute bottom-0 right-3  transform  p-2 text-2xl text-black transition-transform duration-300 ease-in-out hover:scale-125"
         >
           {">"}
         </button>
